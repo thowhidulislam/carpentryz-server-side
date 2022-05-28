9b9cbc09d2dfbf3d16f998f11ac9694f04678499
thowhidulislam <thowhid.ndc14@gmail.com>
2022-05-28 21:00:31 +0600

    removing user from database by adming

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express('');
 const cors = require('cors')
 require('dotenv').config()
+jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 const port = process.env.PORT || 5000
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
@@ -12,6 +13,21 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PAS}@cluster0.5zgoy.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized' })
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded
+        next()
+    })
+}
 
 async function run() {
     try {
@@ -34,6 +50,17 @@ async function run() {
             })
             res.send({ clientSecret: paymentIntent.client_secret })
         })
+
+        const verifyAdmin = async (req, res, next) => {
+            const requester = req.decoded.email
+            const requesterAccount = await userCollection.findOne({ email: requester })
+            if (requesterAccount.role === 'admin') {
+                next()
+            }
+            else {
+                res.status(403).send({ message: 'Forbidden' })
+            }
+        }
 
         app.get('/products', async (req, res) => {
             const query = {}
@@ -138,10 +165,10 @@ async function run() {
             res.send({ success: true, result, order })
         })
 
-        app.delete('/order/:email', async (req, res) => {
-            const email = req.params.email
-            const query = { email: email }
-            const result = await orderCollection.deleteOne(query)
+        app.delete('/order/user/:id', async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: ObjectId(id) }
+            const result = await orderCollection.deleteOne(filter)
             res.send({ success: true, result })
         })
 
@@ -166,36 +193,60 @@ async function run() {
 
         //user
         app.get('/user', async (req, res) => {
-            const query = {}
-            const allUsers = await userCollection.find(query).toArray()
+            const allUsers = await userCollection.find().toArray()
             res.send({ success: true, allUsers })
             console.log(allUsers)
         })
         app.get('/user/:email', async (req, res) => {
             const email = req.params.email
-            console.log(email)
             const query = { email: email }
-            console.log(query)
             const result = await userCollection.findOne(query)
+            res.send({ success: true, result })
             console.log(result)
+        })
+        app.put('/user/profile/:email', async (req, res) => {
+            const email = req.params.email
+            const userDetails = req.body
+            const filter = { email: email }
+            const options = { upsert: true }
+            const updateDoc = {
+                $set: userDetails
+            }
+            const result = await userCollection.updateOne(filter, updateDoc, options)
             res.send({ success: true, result })
         })
-        app.put('/user/:email', async (req, res) => {
+        app.put('/user/create/:email', async (req, res) => {
             const email = req.params.email
             const user = req.body
             const filter = { email: email }
             const options = { upsert: true }
             const updateDoc = {
-                $set: user
+                $set: { user }
             }
             const result = await userCollection.updateOne(filter, updateDoc, options)
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '7d'
+            })
+            console.log(token)
+            res.send({ result, token })
+        })
+
+        app.delete('/admin/delete/:email', async (req, res) => {
+            const email = req.params.email
+            const filter = { email: email }
+            const result = await userCollection.deleteOne(filter)
             res.send({ success: true, result })
         })
         //admin
+        app.get('/admin/allUsers', async (req, res) => {
+            const result = await userCollection.find().toArray()
+            res.send({ success: true, result })
+            console.log(result)
+        })
         app.get('/admin/:email', async (req, res) => {
             const email = req.params.email
             const user = await userCollection.findOne({ email: email })
-            const isAdmin = user.role === 'admin'
+            const isAdmin = user?.role === 'admin'
             res.send({ success: true, admin: isAdmin })
             console.log(isAdmin)
         })
